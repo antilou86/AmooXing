621d62ff405da6849b2a19fd23c5bb203cd742f4
luis guzman <antilou86@gmail.com>
2020-04-12 20:09:56 -0400

    workin on it

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
+const cors = require('cors')
+const server = express();
 
 const hostname = '127.0.0.1'; // the local computer where the server is running
 const port = process.env.PORT || 7777; // a port we'll use to watch for traffic
 
-const server = express();
-
+server.use(cors())
 server.get('/', (req,res) => {
     console.log("200: GET request to /")
     const fruits = [ //all this will eventually pulled from the DB
@@ -19,6 +20,10 @@ server.get('/', (req,res) => {
         }
     ] 
     res.status(200).json({fruits, message:"Yup, it's working.."})
+})
+//handles 404s
+server.use(function(req,res){
+    res.status(404).send("whatever you just did isn't a thing.")
 })
 
 server.listen(port, hostname, () => {

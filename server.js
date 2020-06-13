783bd6cef904c418905e25e2267b05eff36df548
luis guzman <antilou86@gmail.com>
2020-06-13 19:33:21 -0400

    added db pool info. testing this now.

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const server = express();
 const router = express.Router()
 const port = process.env.PORT || 7777
 
+//db pool
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 //middleware for auth
 server.use(require("cors")());
@@ -17,6 +25,20 @@ server.get("/", (req, res) => {
     console.log("SUCCESSFUL GET TO '/' ")
     res.status(200).json("Yup, it's working..");    
   });
+
+//create a db endpoint for checking test_tables
+server.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 //routers
 server.use(require("./endpoints/routers/users"));
